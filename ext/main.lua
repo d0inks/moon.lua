e1935aa963676183947fa2ca63085ab97c2894d6
doink <aricjacob123@gmail.com>
2026-01-11 07:45:16 -0600

    Implement game-specific script loading
    Added game-specific loading for Blox Strike and Counter Blox.

--- ext/main.lua
+++ ext/main.lua
@@ -1,7 +1,9 @@
 if game.PlaceId == 114234929420007 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/d0inks/moon.lua/refs/heads/main/ext/bloxstrike.lua"))() --bloxstrike
+    print("Loaded Blox Strike")
 elseif game.PlaceId == 301549746 or game.PlaceId == 1480424328 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/d0inks/moon.lua/refs/heads/main/ext/counterblox.lua"))() -- counterblox
+    print("Loaded Counter Blox")
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/d0inks/moon.lua/refs/heads/main/ext/universal.lua"))() --universal
     print("Moon.lua Universal")
